2c8d7180548088e7b3f443c0dc70c4d3adae851b
RobelSamson <robelsamson@ucsb.edu>
2021-05-29 13:47:52 -0700

    Added a change in line 7 of our code

--- Testing R.R
+++ Testing R.R
@@ -3,3 +3,6 @@ print("This file was created in Rstudio")
 print("now this project will be live on github")
 
 print("I will be an expert programmer soon")
+
+print("The Lakers will repeat as NBA champions and I will have a new job before the end of this year")
+
